6b99aa702878affc2245925cd0cbcf1ff6629188
eightynine77 <theskyisgrey80@proton.me>
2025-10-22 08:53:33 +0700

    Implement user confirmation for system shutdown
    Added a confirmation loop for shutdown with user input.

--- shutdown-now.sh
+++ shutdown-now.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
+
+while true; do
+    echo do you want to shutdown this system?
+    echo ""
+    echo ""
+    echo "[a] yes"
+    echo "[s] no"
+    echo -n "> "
+    read -n1 choice
+    echo
+
+    case "$choice" in
+        a|A) 
+echo shutting down system...
 systemctl poweroff
+break 
+;;  
+        s|S) break ;;     
+        *) 
+echo "invalid choice"
+continue 
+;;    
+    esac
+done
 
 #this script shuts down your system
-
 #NOTE: you must use a linux distro that supports systemctl or have systemctl installed
